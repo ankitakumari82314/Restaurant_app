7c6a8113b9f8d539994bda8d9d916e07d4ad7b7c
Ankita Kumari <akumari@ghx.com>
2023-10-29 23:32:51 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { MenuAddFormComponent } from './menu-add-form/menu-add-form.component';
+import { MenuListComponent } from './menu-list/menu-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'admin',
+    component: AdminComponent,
+  },
+  {
+    path:'admin/menu', 
+    component: MenuComponent
+  },
+  {
+    path:'admin/restaurant',
+    component: RestaurantComponent
+  },
+  {
+    path:'admin/menu/add',
+    component: MenuAddFormComponent
+  },
+  {
+    path:'list',
+    component: MenuListComponent
+  },
+  {
+    path:'user',
+    component: UserComponent
+  },
+  {
+    path:'',
+    component: HomeComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { AdminModule } from './admin/admin.module';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { MenuModule } from './menu/menu.module';
+import { MenuAddFormComponent } from './menu-add-form/menu-add-form.component';
+import { MenuListModule } from './menu-list/menu-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    MenuAddFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AdminModule,
+    MenuModule,
+    MenuListModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
